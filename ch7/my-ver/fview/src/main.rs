98dbbb684edde1fc18c52fddb1e246906e225fc1
paf <pafus23@protonmail.com>
2021-11-13 12:40:35 -0500

    Ch7. fview example updates

--- ch7/my-ver/fview/src/main.rs
+++ ch7/my-ver/fview/src/main.rs
@@ -9,27 +9,20 @@ fn main() {
   let arg1 = env::args().nth(1);
 
   let fname = arg1.expect("usage: fview FILENAME");
-
   let mut f = File::open(&fname).expect("Unable to open file.");
-  const BYTES_PER_LINE: usize = 16;
-  const INPUT: &'static [u8] = br#"
-  fn main() {
-    println!("Hello, world!");
-  }"#;
+  let mut pos = 0;
+  let mut buffer = [0; BYTES_PER_LINE];
 
-  fn main() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = vec!();
-    INPUT.read_to_end(&mut buffer)?;
-    let mut position_in_input = 0;
-    for line in buffer.chunks(BYTES_PER_LINE){
-      print!("[0x{:08x}] ", position_in_input);
-      for byte in line {
-        print!("{:02x} ", byte);
+  while let Ok(_) = f.read_exact(&mut buffer) {
+    print!("[0x{:08x}] ", pos);
+    for byte in &buffer {
+      match *byte {
+        0x00 => print!(". "),
+        0xff => print!("## "),
+        _ => print!("{:02x} ", byte),
       }
-      println!();
-      position_in_input += BYTES_PER_LINE;
     }
-    Ok(())
+    println!("");
+    pos += BYTES_PER_LINE;
   }
-  main();
 }
